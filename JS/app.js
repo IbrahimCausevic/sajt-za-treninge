86572142a9bb5a2399e331175a9f39922934fd7c
IbrahimCausevic <icausevic25@gmail.com>
2023-06-03 21:20:54 +0200

    ramena dodata

---
+++ JS/app.js
@@ -0,0 +1,22 @@
+function checkPassword() {
+  var password = document.getElementById("password").value;
+  if (password === "123") {
+    window.location.href = "index2.html";
+  } else {
+    alert("Incorrect password. Please try again.");
+  }
+}
+
+function goBack() {
+  window.history.back();
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
