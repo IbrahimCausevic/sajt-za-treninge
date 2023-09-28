211c8751efe5bfa32ccb1b359b186d20090f0351
Ibrahim Čaušević <92829361+IbrahimCausevic@users.noreply.github.com>
2023-09-28 18:45:12 +0200

    update

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 function checkPassword() {
   var password = document.getElementById("password").value;
-  if (password === "123") {
+  if (password === "J1994S1999B") {
     window.location.href = "../calendar";
   } else {
     alert("Incorrect password. Please try again.");
@@ -9,7 +9,7 @@ function checkPassword() {
 
 function WPassword() {
   var password = document.getElementById("password").value;
-  if (password === "321") {
+  if (password === "J941999SB") {
     window.location.href = "../calendarWL/";
   } else {
     alert("Incorrect password. Please try again.");
@@ -18,8 +18,17 @@ function WPassword() {
 
 function DietPassword() {
   var password = document.getElementById("password").value;
-  if (password === "456") {
-    window.location.href = "../dietplan/";
+  if (password === "J999MM994SB") {
+    window.location.href = "../diet/";
+  } else {
+    alert("Incorrect password. Please try again.");
+  }
+}
+
+function DietPasswordWL() {
+  var password = document.getElementById("password").value;
+  if (password === "J994WL999SB") {
+    window.location.href = "../dietWL/";
   } else {
     alert("Incorrect password. Please try again.");
   }
